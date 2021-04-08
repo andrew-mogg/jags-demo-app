5a27c60bfb26260fd3988d4357393b86a4cbc00f
Andrew Mogg <17276059+andrew-mogg@users.noreply.github.com>
2021-04-07 22:47:53 -0400

    formatting updates

--- src/app/quarterbacks/quarterbacks.component.ts
+++ src/app/quarterbacks/quarterbacks.component.ts
@@ -1,41 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
 
-import { IQuarterback } from '../models/IQuarterback';
-import { QuarterbackService } from '../services/quarterback.service';
+import { IQuarterback } from "../models/IQuarterback";
+import { QuarterbackService } from "../services/quarterback.service";
 
 @Component({
-  selector: 'app-quarterbacks',
-  templateUrl: './quarterbacks.component.html',
-  styleUrls: ['./quarterbacks.component.scss']
+  selector: "app-quarterbacks",
+  templateUrl: "./quarterbacks.component.html",
+  styleUrls: ["./quarterbacks.component.scss"],
 })
 export class QuarterbacksComponent implements OnInit {
-
   selectedQb: number = 0;
   quarterbacks: IQuarterback[] = [];
   activeQb: IQuarterback;
-  displayedColumns: string[] = ['week', 'opponentName', 'gameDate', 'yardsPerAttempt', 'completionPercentage', 'completions', 'attempts', 'passingYards', 'passingTds', 'interceptions', 'sacks', 'rushYards', 'rushTds'];
+  displayedColumns: string[] = [
+    "week",
+    "opponentName",
+    "gameDate",
+    "yardsPerAttempt",
+    "completionPercentage",
+    "completions",
+    "attempts",
+    "passingYards",
+    "passingTds",
+    "interceptions",
+    "sacks",
+    "rushYards",
+    "rushTds",
+  ];
   dataSource = new MatTableDataSource<IQuarterback>(null);
 
-  constructor(private quarterbackService: QuarterbackService) { }
+  constructor(private quarterbackService: QuarterbackService) {}
 
   ngOnInit() {
     this.getQuarterbacks();
   }
 
   getQuarterbacks() {
-    this.quarterbackService.getQuarterbacks()
-      .subscribe(quarterbacks => (
-        this.quarterbacks = quarterbacks,
-        this.activeQb = this.quarterbacks[0],
-        this.selectedQb = this.activeQb.playerId)
-        );
+    this.quarterbackService
+      .getQuarterbacks()
+      .subscribe(
+        (quarterbacks) => (
+          (this.quarterbacks = quarterbacks),
+          (this.activeQb = this.quarterbacks[0]),
+          (this.selectedQb = this.activeQb.playerId)
+        )
+      );
   }
 
   updateTable(event: any) {
-    this.activeQb = this.quarterbacks.filter(qb => qb.playerId == event.value)[0];
-    console.log(this.selectedQb)
-
+    this.activeQb = this.quarterbacks.filter(
+      (qb) => qb.playerId == event.value
+    )[0];
+    console.log(this.selectedQb);
   }
-
 }
--- src/app/services/quarterback.service.ts
+++ src/app/services/quarterback.service.ts
@@ -1,47 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Observable, of } from 'rxjs';
+import { Observable, of } from "rxjs";
 
-import { IQuarterback, IGameData } from '../models/IQuarterback';
-import { ISeasonStats } from '../models/ISeasonStats';
-import { QUARTERBACKS } from './data';
+import { IQuarterback, IGameData } from "../models/IQuarterback";
+import { ISeasonStats } from "../models/ISeasonStats";
+import { QUARTERBACKS } from "./data";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class QuarterbackService {
   private quarterbacks: IQuarterback[] = [];
 
-  constructor() { }
+  constructor() {}
 
   getQuarterbacks(): Observable<IQuarterback[]> {
     QUARTERBACKS.forEach((qb, i) => {
       const gameDataList: IGameData[] = [];
-      qb.rows.forEach(q => {
-          const gameData: IGameData = {
-            playerId: Number(q[0]),
-            name: String(q[1]),
-            playerImage: String(q[2]),
-            seasonYear: Number(q[3]),
-            week: Number(q[4]),
-            gameDate: String(q[5]),
-            teamName: String(q[6]),
-            teamImage: String(q[7]),
-            opponentName: String(q[8]),
-            opponentImage: String(q[9]),
-            attempts:    Number(q[10]),
-            completions:  Number(q[11]),
-            sacks:    Number(q[12]),
-            interceptions:  Number(q[13]),
-            passingYards:    Number(q[14]),
-            passingTds:  Number(q[15]),
-            rushAttempts:    Number(q[16]),
-            rushYards:  Number(q[17]),
-            rushTds:    Number(q[18])
-          }
-          gameData.completionPercentage = gameData.completions / gameData.attempts;
-          gameData.yardsPerAttempt = Math.ceil((gameData.passingYards / gameData.attempts) * 10) / 10;
-          gameDataList.push(gameData);
+      qb.rows.forEach((q) => {
+        const gameData: IGameData = {
+          playerId: Number(q[0]),
+          name: String(q[1]),
+          playerImage: String(q[2]),
+          seasonYear: Number(q[3]),
+          week: Number(q[4]),
+          gameDate: String(q[5]),
+          teamName: String(q[6]),
+          teamImage: String(q[7]),
+          opponentName: String(q[8]),
+          opponentImage: String(q[9]),
+          attempts: Number(q[10]),
+          completions: Number(q[11]),
+          sacks: Number(q[12]),
+          interceptions: Number(q[13]),
+          passingYards: Number(q[14]),
+          passingTds: Number(q[15]),
+          rushAttempts: Number(q[16]),
+          rushYards: Number(q[17]),
+          rushTds: Number(q[18]),
+        };
+        gameData.completionPercentage =
+          gameData.completions / gameData.attempts;
+        gameData.yardsPerAttempt =
+          Math.ceil((gameData.passingYards / gameData.attempts) * 10) / 10;
+        gameDataList.push(gameData);
       });
 
       const quarterback: IQuarterback = {
@@ -50,31 +52,51 @@ export class QuarterbackService {
         playerImage: gameDataList[0].playerImage,
         seasonYear: gameDataList[0].seasonYear,
         teamImage: gameDataList[0].teamImage,
-        gameData: gameDataList
-      }
-      quarterback.seasonStats = this.calculateSeasonStats(quarterback.gameData)
+        gameData: gameDataList,
+      };
+      quarterback.seasonStats = this.calculateSeasonStats(quarterback.gameData);
 
       this.quarterbacks.push(quarterback);
-      console.log(this.quarterbacks)
-    })
+      console.log(this.quarterbacks);
+    });
     return of(this.quarterbacks);
   }
 
-  private calculateSeasonStats(gameStats: IGameData[]) : ISeasonStats {
+  private calculateSeasonStats(gameStats: IGameData[]): ISeasonStats {
     const seasonStats: ISeasonStats = {
       year: gameStats[0].seasonYear,
-      attempts: gameStats.filter(gs => gs.attempts).reduce((a, b) => a + b.attempts, 0),
-      completions: gameStats.filter(gs => gs.completions).reduce((a, b) => a + b.completions, 0),
-      sacks: gameStats.filter(gs => gs.sacks).reduce((a, b) => a + b.sacks, 0),
-      interceptions: gameStats.filter(gs => gs.interceptions).reduce((a, b) => a + b.interceptions, 0),
-      passingYards: gameStats.filter(gs => gs.passingYards).reduce((a, b) => a + b.passingYards, 0),
-      passingTds: gameStats.filter(gs => gs.passingTds).reduce((a, b) => a + b.passingTds, 0),
-      rushAttempts: gameStats.filter(gs => gs.attempts).reduce((a, b) => a + b.rushAttempts, 0),
-      rushYards: gameStats.filter(gs => gs.attempts).reduce((a, b) => a + b.rushYards, 0),
-      rushTds: gameStats.filter(gs => gs.attempts).reduce((a, b) => a + b.rushTds, 0),
-    }
-    seasonStats.completionPercentage = seasonStats.completions / seasonStats.attempts;
-    seasonStats.yardsPerAttempt = Math.ceil((seasonStats.passingYards / seasonStats.attempts) * 10) / 10;;
+      attempts: gameStats
+        .filter((gs) => gs.attempts)
+        .reduce((a, b) => a + b.attempts, 0),
+      completions: gameStats
+        .filter((gs) => gs.completions)
+        .reduce((a, b) => a + b.completions, 0),
+      sacks: gameStats
+        .filter((gs) => gs.sacks)
+        .reduce((a, b) => a + b.sacks, 0),
+      interceptions: gameStats
+        .filter((gs) => gs.interceptions)
+        .reduce((a, b) => a + b.interceptions, 0),
+      passingYards: gameStats
+        .filter((gs) => gs.passingYards)
+        .reduce((a, b) => a + b.passingYards, 0),
+      passingTds: gameStats
+        .filter((gs) => gs.passingTds)
+        .reduce((a, b) => a + b.passingTds, 0),
+      rushAttempts: gameStats
+        .filter((gs) => gs.attempts)
+        .reduce((a, b) => a + b.rushAttempts, 0),
+      rushYards: gameStats
+        .filter((gs) => gs.attempts)
+        .reduce((a, b) => a + b.rushYards, 0),
+      rushTds: gameStats
+        .filter((gs) => gs.attempts)
+        .reduce((a, b) => a + b.rushTds, 0),
+    };
+    seasonStats.completionPercentage =
+      seasonStats.completions / seasonStats.attempts;
+    seasonStats.yardsPerAttempt =
+      Math.ceil((seasonStats.passingYards / seasonStats.attempts) * 10) / 10;
 
     return seasonStats;
   }
